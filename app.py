476c99637e2c2164028cb46a20935a489f9dc623
Rupa Prasanna Challa <rupaprasannachalla@gmail.com>
2026-07-02 23:10:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,31 +6,40 @@ import os
 app = Flask(__name__)
 
 # Load trained model
-model = pickle.load(open('xgb_loan_model.pkl', 'rb'))
+with open("xgb_loan_model.pkl", "rb") as f:
+    model = pickle.load(f)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
-    gender = int(request.form['Gender'])
-    married = int(request.form['Married'])
-    dependents = int(request.form['Dependents'])
-    education = int(request.form['Education'])
-    self_employed = int(request.form['Self_Employed'])
-    applicant_income = float(request.form['ApplicantIncome'])
-    coapplicant_income = float(request.form['CoapplicantIncome'])
-    loan_amount = float(request.form['LoanAmount'])
-    loan_amount_term = float(request.form['Loan_Amount_Term'])
-    credit_history = float(request.form['Credit_History'])
-    property_area = int(request.form['Property_Area'])
+    gender = int(request.form["Gender"])
+    married = int(request.form["Married"])
+    dependents = int(request.form["Dependents"])
+    education = int(request.form["Education"])
+    self_employed = int(request.form["Self_Employed"])
+    applicant_income = float(request.form["ApplicantIncome"])
+    coapplicant_income = float(request.form["CoapplicantIncome"])
+    loan_amount = float(request.form["LoanAmount"])
+    loan_amount_term = float(request.form["Loan_Amount_Term"])
+    credit_history = float(request.form["Credit_History"])
+    property_area = int(request.form["Property_Area"])
 
-    features = np.array([[gender, married, dependents, education,
-                          self_employed, applicant_income,
-                          coapplicant_income, loan_amount,
-                          loan_amount_term, credit_history,
-                          property_area]])
+    features = np.array([[
+        gender,
+        married,
+        dependents,
+        education,
+        self_employed,
+        applicant_income,
+        coapplicant_income,
+        loan_amount,
+        loan_amount_term,
+        credit_history,
+        property_area
+    ]])
 
     prediction = model.predict(features)
 
@@ -39,8 +48,7 @@ def predict():
     else:
         result_text = "Sorry, you are not eligible for the loan."
 
-    return render_template('result.html', prediction_text=result_text)
-
+    return render_template("result.html", prediction_text=result_text)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
